b0db0f2f1a353cc6bcb4bf8f0cb06c05118a54de
Hanul <chu9400@naver.com>
2023-10-20 18:11:33 +0900

    사이트 완료

--- src/constants/index.js
+++ src/constants/index.js
@@ -52,43 +52,32 @@ export const skillText = [
 
 export const siteText = [
     {
-        text: ["make", "site compliant with", "webstandard"],
-        title: "비트를 이용한 사이트 제작",
-        code: "https://github.com/webstoryboy/port2023-vite",
-        view: "https://port2023-vite.netlify.app",
-        info: [
-            "site coding",
-            "production period : two days",
-            "use stack : HTML5/CSS3, CSS Variable, Vite",
-        ],
-    },
-    {
         text: ["make", "site compliant with", "react.js"],
         title: "리액트를 이용한 사이트 제작",
-        code: "https://github.com/webstoryboy/port2023-react",
-        view: "https://port2023-react.netlify.app",
+        code: "https://github.com/chu9400/port2023-react",
+        view: "#",
         info: [
             "site coding",
-            "production period : two days",
+            "production period : three days",
             "use stack : HTML5/CSS3, CSS Variable, Vite",
         ],
     },
     {
         text: ["make", "site compliant with", "vue.js"],
         title: "뷰를 이용한 사이트 제작",
-        code: "https://github.com/webstoryboy/port2023-vue",
-        view: "https://port2023-vue.netlify.app",
+        code: "https://github.com/chu9400/port2023-vue",
+        view: "https://chu9400-port2023-vue.netlify.app/",
         info: [
             "site coding",
-            "production period : two days",
+            "production period : three days",
             "use stack : HTML5/CSS3, Scss Variable, vue",
         ],
     },
     {
         text: ["make", "site compliant with", "next.js"],
         title: "넥스트를 이용한 사이트 제작",
-        code: "https://github.com/webstoryboy/port2023-next",
-        view: "https://port2023-next.netlify.app",
+        code: "https://github.com/chu9400/port2023-next",
+        view: "/",
         info: [
             "site coding",
             "production period : two days",
